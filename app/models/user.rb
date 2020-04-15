962a89d422a4e3d806b1a96203780764843c6144
Pezz <pezz.fets@gmail.com>
2020-04-15 07:43:09 +0000

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,9 +35,7 @@ class User < ApplicationRecord
   end
   
   def like(other_micropost)
-    unless self == other_micropost
       self.favorites.find_or_create_by(micropost_id: other_micropost.id)
-    end
   end
   
   def unlike(other_micropost)
